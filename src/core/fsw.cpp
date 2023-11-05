f157100c76a2a84eb7d6552870d734989268c7dd
Cameron Goddard <camerongoddard@me.com>
2023-11-05 00:11:03 -0400

    add verbose and debug flags

--- src/core/fsw.cpp
+++ src/core/fsw.cpp
@@ -2,7 +2,20 @@
 
 
 void Flight::execute() {
-    // sleep_ms(5000);
+#ifdef DEBUG
+    sleep_ms(3000);
+#endif
+    
+#ifdef VERBOSE
+    printf("------------BEGIN LOOP------------\n");
+    printf("Mode: %s\n", state::flight::mode->name().c_str());
+    printf("Armed: %d\n", state::flight::armed);
+#endif
+    
     state::flight::mode->execute();
     state::flight::mode->transition();
+
+#ifdef VERBOSE
+    printf("-------------END LOOP-------------\n\n");
+#endif
 }
