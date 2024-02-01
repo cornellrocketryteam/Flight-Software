fc5994291bfadf14284e9a23b96cd74de40ba76b
Cameron Goddard <camerongoddard@me.com>
2024-01-31 19:08:16 -0500

    format

--- src/constants.hpp
+++ src/constants.hpp
@@ -8,7 +8,7 @@ namespace constants {
 
     constexpr uint16_t arming_altitude = 25;        // meters           TODO: CHANGE TO 1524 FOR LAUNCH
     constexpr uint16_t main_deploy_altitude = 40;   // meters           TODO: CHANGE TO 2286 FOR LAUNCH
-    constexpr uint8_t max_failed_reads = 10; 
+    constexpr uint8_t max_failed_reads = 10;
 
     constexpr uint8_t accel_threshold = 3;          // m/s^2            TODO: CHANGE FOR LAUNCH
 
