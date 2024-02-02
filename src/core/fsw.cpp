2862b67705f7d7fb4979b4b05a7c2cc189b01aa9
Cameron Goddard <camerongoddard@me.com>
2024-02-02 14:22:53 -0500

    add units to verbose output

--- src/core/fsw.cpp
+++ src/core/fsw.cpp
@@ -12,26 +12,26 @@ void Flight::execute() {
     printf("Altitude armed: %d\n\n", state::flight::altitude_armed);
 
     printf("ALTIMETER: %d\n", state::alt::status);
-    printf("Altitude: %.3f\n", state::alt::altitude);
-    printf("Pressure: %.3f\n\n", state::alt::pressure);
+    printf("Altitude: %.3f m\n", state::alt::altitude);
+    printf("Pressure: %.3f hPa\n\n", state::alt::pressure);
 
     printf("IMU: %d\n", state::imu::status);
-    printf("Gyro X: %.3f\n", state::imu::gyro_x);
-    printf("Gyro Y: %.3f\n", state::imu::gyro_y);
-    printf("Gyro Z: %.3f\n\n", state::imu::gyro_z);
+    printf("Gyro X: %.3f m/s^2\n", state::imu::gyro_x);
+    printf("Gyro Y: %.3f m/s^2\n", state::imu::gyro_y);
+    printf("Gyro Z: %.3f m/s^2\n\n", state::imu::gyro_z);
 
-    printf("Mag X: %.3f\n", state::imu::mag_x);
-    printf("Mag Y: %.3f\n", state::imu::mag_y);
-    printf("Mag Z: %.3f\n\n", state::imu::mag_z);
+    printf("Mag X: %.3f m/s^2\n", state::imu::mag_x);
+    printf("Mag Y: %.3f m/s^2\n", state::imu::mag_y);
+    printf("Mag Z: %.3f m/s^2\n\n", state::imu::mag_z);
 
     printf("ACCELEROMETER: %d\n", state::accel::status);
-    printf("Accel X: %.3f\n", state::accel::accel_x);
-    printf("Accel Y: %.3f\n", state::accel::accel_y);
-    printf("Accel Z: %.3f\n\n", state::accel::accel_z);
+    printf("Accel X: %.3f m/s^2\n", state::accel::accel_x);
+    printf("Accel Y: %.3f m/s^2\n", state::accel::accel_y);
+    printf("Accel Z: %.3f m/s^2\n\n", state::accel::accel_z);
 
     printf("THERMOMETER: %d\n", state::therm::status);
-    printf("Temperature: %.3f\n", state::therm::temp);
-    printf("Humidity: %.3f\n\n", state::therm::humidity);
+    printf("Temperature: %.3f C\n", state::therm::temp);
+    printf("Humidity: %.3f %%\n\n", state::therm::humidity);
 
     printf("SD: %d\n\n", state::sd::init);
 
