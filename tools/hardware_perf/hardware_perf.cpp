c7a7e8f1967064169f61219669d458a21ebb7235
Cameron Goddard <camerongoddard@me.com>
2025-01-09 17:35:47 -0500

    time telem functions

--- tools/hardware_perf/hardware_perf.cpp
+++ tools/hardware_perf/hardware_perf.cpp
@@ -24,6 +24,7 @@
 #include <functional>
 #include <tuple>
 #include <array>
+#include <bitset>
 
 #define NUM_RUNS 200
 
@@ -49,6 +50,9 @@ FATFS fs;
 FRESULT fr;
 char buffer[205];
 
+uint8_t packet[constants::rfm_packet_size];
+char throwaway_buffer[200];
+
 void sd_begin() {
     fr = f_mount(&fs, "", 1);
     if (FR_OK != fr) {
@@ -72,6 +76,16 @@ void sd_write() {
     fr = f_close(&fil);
     if (FR_OK != fr) {
         printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+}
+
+void rfm_transmit() {
+    uart_write_blocking(UART_PORT, (const uint8_t *)packet, constants::rfm_packet_size);
+}
+
+void umb_transmit() {
+    for (uint i = 0; i < constants::umb_packet_size; ++i) {
+        sprintf(throwaway_buffer, "%s ", std::bitset<8>(static_cast<uint8_t>(packet[i])).to_string().c_str());
     }
 }
 
@@ -161,6 +175,17 @@ uint64_t time_storage() {
     return storage_total_sum;
 }
 
+uint64_t time_telem() {
+    printf("============ Telem ============\n\n");
+    uint64_t telem_total_sum = 0;
+
+    telem_total_sum += run_sensor_test("RFM transmit()", [&]() { rfm_transmit(); });
+    telem_total_sum += run_sensor_test("UMB transmit()", [&]() { umb_transmit(); });
+
+    printf("\n");
+    return telem_total_sum;
+}
+
 int main() {
     stdio_init_all();
 
@@ -175,11 +200,13 @@ int main() {
 
     uint64_t sensors_total_sum = time_sensors();
     uint64_t storage_total_sum = time_storage();
+    uint64_t telem_total_sum = time_telem();
 
     printf("===============================\n\n");
 
     printf("Sensors total sum: %llu us\n", sensors_total_sum);
     printf("Storage total sum: %llu us\n", storage_total_sum);
+    printf("Telem total sum: %llu us\n", telem_total_sum);
 
     return 0;
 }
