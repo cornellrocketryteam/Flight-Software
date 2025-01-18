11822608d8f3973b8bb6080ec740d4b6e0c3135f
Cameron Goddard <camerongoddard@me.com>
2025-01-17 20:13:40 -0500

    fix umbilical telem

--- src/telem/telem.cpp
+++ src/telem/telem.cpp
@@ -78,8 +78,14 @@ void Umbilical::transmit() {
     memcpy(&packet[10], &state::rfm::init, sizeof(bool));
     memcpy(&packet[11], &state::rfm::init, sizeof(bool));
 
+    uint32_t test = 0b01000010001010000000000000000001;
+
+    memcpy(&packet[12], &test, sizeof(float));
+    memcpy(&packet[16], &test, sizeof(float));
+    memcpy(&packet[20], &test, sizeof(float));
+
     for (uint i = 0; i < constants::umb_packet_size; ++i) {
-        printf("%s", std::bitset<8>(static_cast<uint8_t>(packet[i])).to_string().c_str());
+        printf("%c", packet[i]);
     }
     printf("\n");
 }
