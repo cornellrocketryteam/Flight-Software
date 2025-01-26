2df1d43af9abd7abde0a9deaf8039e938b01cdea
Cameron Goddard <camerongoddard@me.com>
2025-01-26 16:46:44 -0500

    add buzzer unit test

---
+++ test/buzzer/buzzer.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
+#include "pins.hpp"
+
+uint buzzer_slice_num = pwm_gpio_to_slice_num(ARM_OUT);
+
+void buzz(const uint32_t length) {
+	pwm_set_enabled(buzzer_slice_num, true);
+	sleep_ms(length);
+	pwm_set_enabled(buzzer_slice_num, false);
+	sleep_ms(30);
+}
+
+int main() {
+    stdio_init_all();
+
+	init_pins();
+
+	while (true) {
+		buzz(250);
+		buzz(100);
+		buzz(50);
+		buzz(50);
+		buzz(20);
+		buzz(70);
+		buzz(10);
+		buzz(100);
+		buzz(250);
+	}
+	
+	return 0;
+}
